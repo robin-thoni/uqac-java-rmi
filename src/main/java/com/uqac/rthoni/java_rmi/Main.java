735fb1045398a7ad09f5b6c22a67cbd3691fe54d
Robin THONI <robin@rthoni.com>
2016-09-15 16:36:36 -0400

    added main class; ignore bin files

---
+++ src/main/java/com/uqac/rthoni/java_rmi/Main.java
@@ -0,0 +1,12 @@
+package com.uqac.rthoni.java_rmi;
+
+/**
+ * Created by robin on 9/15/16.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        System.exit(0);
+    }
+}
